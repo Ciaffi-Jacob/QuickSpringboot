871a78ba8ec97f03c7bb646d1f0c59285b4af0c4
Ciaffi-Jacob <ciaffi.jacob@gmail.com>
2023-08-28 21:07:23 -0400

    notes added

--- src/main/java/com/example/demo/PersonConfig.java
+++ src/main/java/com/example/demo/PersonConfig.java
@@ -7,13 +7,17 @@ import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
 
+    //indicates the class is a source of bean definition
 @Configuration
 public class PersonConfig {
+        //automatic dependency injection
     @Autowired
     private PersonRepository repository;
 
+        //Pre filles database with 2 person objects
     @PostConstruct
     public void setup() {
+                //constructors
         Person person1 = new Person();
         person1.setFirstName("Jake");
         person1.setLastName("Ciaffi");
@@ -22,6 +26,7 @@ public class PersonConfig {
         person2.setFirstName("John");
         person2.setLastName("Smith");
 
+            //save people to database
         repository.saveAll(Arrays.asList(person1, person2));
     }
 }
